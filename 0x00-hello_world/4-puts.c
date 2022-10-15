c2aec70ba58f891d1c98928247ae14fb2477b18f
iam-gechy <onwukajudyth@gmail.com>
2022-10-14 19:25:57 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry
- * puts - replaces printf
- * return is 0
+ * main - Entry point
+ *
+ * Return: Always 0
  */
 int main(void)
-/* entry point */
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
-/* return 0 means that the program executed successfully. */
 }
