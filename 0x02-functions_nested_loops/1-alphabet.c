1a3a35bf3cdbfecedea833f76eea9ab39057cf7c
iam-gechy <onwukajudyth@gmail.com>
2022-10-23 01:23:42 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,14 @@
+#include "main.h"
 
+/**
+ * print_alphabet - print the alphabet, in lowercase, followed by new line.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
