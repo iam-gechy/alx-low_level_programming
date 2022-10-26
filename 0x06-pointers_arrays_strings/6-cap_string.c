83d0982fa68cd1924bfa3494059ac8e849bff442
iam-gechy <onwukajudyth@gmail.com>
2022-10-26 14:02:44 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalize all words of a string.
+ * @s: string
+ *
+ * Return: all capitalized words
+ */
+
+char *cap_string(char *s)
+{
+	int i, x;
+	int cap = 32;
+	int separators[] = {',', ';', '.', '?', '"',
+			    '(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - cap;
+		}
+		cap = 0;
+		for (x = 0; x <= 12; x++)
+		{
+			if (s[i] == separators[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (s);
+}
