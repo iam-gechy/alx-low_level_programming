956f5e877e8fe633bc17e3e6e22e2d41a1b51a6f
iam-gechy <onwukajudyth@gmail.com>
2022-10-29 07:55:02 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,11 @@ void print_array(int *a, int n)
 
 	for (index = 0; index < n; index++)
 	{
-		printf("%d", a[index]);
-		if (index != (n - 1))
-		{
-			printf(",");
-		}
+		if (index == 0)
+			printf("%d", a[index]);
+		else
+			printf(", %d", a[index]);
 	}
+
 	putchar('\n');
 }
