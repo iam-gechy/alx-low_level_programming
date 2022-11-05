50c857d55a5046420509866de2c47094c82c3b16
iam-gechy <onwukajudyth@gmail.com>
2022-11-04 19:53:02 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,69 @@
+nclude <string.h>
+
+#include <ctype.h>
+
+#include <stdio.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
+
+
+/**
+
+   * main - prints program name
+
+    * @argc: no of args
+
+     * @argv: pointer to arguments
+
+      *
+
+       * Return: 0 at exit
+
+        */
+
+
+
+int main(int argc, char *argv[])
+
+{
+
+		int i, j, add = 0;
+
+
+
+			for (i = 1; i < argc; i++)
+
+					{
+
+								for (j = 0; argv[i][j] != '\0'; j++)
+
+											{
+
+															if (!isdigit(argv[i][j]))
+
+																			{
+
+																								printf("Error\n");
+
+																												return (1);
+
+																															}
+
+																	}
+
+										add += atoi(argv[i]);
+
+											}
+
+
+
+				printf("%d\n", add);
+
+
+
+					return (0);
+
+}
