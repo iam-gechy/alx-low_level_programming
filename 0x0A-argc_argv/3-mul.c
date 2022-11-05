334c9b9519b904c26294b34d0efba24ee60f8364
iam-gechy <onwukajudyth@gmail.com>
2022-11-04 19:49:16 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,47 @@
+nclude <stdio.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
+
+
+/**
+
+   * main - prints program name
+
+    * @argc: no of args
+
+     * @argv: pointer to arguments
+
+      *
+
+       * Return: 0 at exit
+
+        */
+
+
+
+int main(int argc, char *argv[])
+
+{
+
+		if (argc < 3)
+
+				{
+
+							printf("Error\n");
+
+									return (1);
+
+										}
+
+
+
+			printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+
+
+				return (0);
+
+}
