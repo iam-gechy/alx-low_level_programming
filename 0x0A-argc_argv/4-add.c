42a9681ecece78b0a74b3734b194f9d0cb31c23e
iam-gechy <onwukajudyth@gmail.com>
2022-11-09 14:00:29 -0800

    4-add review

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * main - prints program name
@@ -10,27 +12,25 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
-
-	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
-	}
+	int i, j, add = 0;
 
 	for (i = 1; i < argc; i++)
 	{
+
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
+
 				return (1);
 			}
 		}
-		sum = sum + atoi(argv[i]);
+
+		add += atoi(argv[i]);
 	}
 
-	printf("%d\n", sum)
-		return (0);
+	printf("%d\n", add);
+
+	return (0);
 }
