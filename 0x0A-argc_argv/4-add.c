736601f80119d9a4c71d9971de94accc399d09b0
iam-gechy <onwukajudyth@gmail.com>
2022-11-09 08:20:28 -0800

    updated 4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,65 +1,33 @@
 #include <string.h>
-
 #include <ctype.h>
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
+#include "main.h"
 
 /**
-
-   * main - prints program name
-
-    * @argc: no of args
-
-     * @argv: pointer to arguments
-
-      *
-
-       * Return: 0 at exit
-
-        */
-
-
+ * main - prints program name
+ * @argc: no of args
+ * @argv: pointer to arguments
+ * Return: 0 at exit
+ */
 
 int main(int argc, char *argv[])
-
 {
+	int i, j, add = 0;
 
-		int i, j, add = 0;
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
 
-
-
-			for (i = 1; i < argc; i++)
-
-					{
-
-								for (j = 0; argv[i][j] != '\0'; j++)
-
-											{
-
-															if (!isdigit(argv[i][j]))
-																	       	{
-																								printf("Error\n");
-
-																												return (1);
-
-																															}
-
-																	}
-
-										add += atoi(argv[i]);
-
-											}
-
-
-
-				printf("%d\n", add);
-
-
-
-					return (0);
-
+	printf("%d\n", add)
+		return (0);
 }
