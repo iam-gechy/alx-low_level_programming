34ac76c05dd7e129feb16815dd9d3753b266dc2e
iam-gechy <onwukajudyth@gmail.com>
2022-11-09 07:08:31 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,47 +1,25 @@
 #include <stdio.h>
-
 #include <string.h>
-
 #include <stdlib.h>
-
-
+#include "main.h"
 
 /**
-
-   * main - prints program name
-
-    * @argc: no of args
-
-     * @argv: pointer to arguments
-
-      *
-
-       * Return: 0 at exit
-
-        */
-
-
+ * main - prints program name
+ * @argc: no of args
+ * @argv: pointer to arguments
+ *
+ * Return: 0 at exit
+ */
 
 int main(int argc, char *argv[])
-
 {
 
-		if (argc < 3)
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-				{
-
-							printf("Error\n");
-
-									return (1);
-
-										}
-
-
-
-			printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
-
-
-				return (0);
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
