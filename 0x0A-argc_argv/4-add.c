13a0f1cc4f774d553ea2564913b289438ceb39a1
iam-gechy <onwukajudyth@gmail.com>
2022-11-09 08:35:05 -0800

    task 4 revision

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,21 +13,27 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int i, j, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		add += atoi(argv[i]);
+		sum = sum + atoi(argv[i]);
 	}
 
-	printf("%d\n", add)
+	printf("%d\n", sum)
 		return (0);
 }
