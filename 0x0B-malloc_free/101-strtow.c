1df33770e684e6256b539ee3ddbb1bdfa762b4d1
iam-gechy <onwukajudyth@gmail.com>
2022-11-09 05:37:26 -0800

    task 6 update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,7 +10,143 @@
 
 char **strtow(char *str)
 {
-	char **a;
+	char **strArr;
+	int wordCount, counter, letter;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	wordCount = word_count(str);
+
+	strArr = (char **)malloc(sizeof(char *) * (wordCount + 1));
+
+	if (!strArr)
+		return (NULL);
+
+	for (counter = 0; counter <= wordCount;)
+	{
+		strArr[counter] = (NULL);
+		counter++;
+	}
+
+	counter = 0;
+	wordCount = 0;
+	letter = 0;
+
+	while (str[counter] != '\0')
+	{
+		if (str[counter] != ' ' && !letter)
+		{
+			strArr[wordCount] = first_word(str + wordCount);
+			if (!strArr[counter])
+			{
+				wordCount--;
+				while (wordCount >= 0)
+					free(*(strArr + wordCount--));
+				free(strArr);
+				return (NULL);
+			}
+			wordCount++;
+			letter = 1;
+		}
+		else if (*(str + counter) == ' ' && letter)
+			letter = 0;
+		counter++;
+
+		if (!wordCount)
+			return (NULL);
+
+		return (strArr);
+
+		if (str[wordCount] != ' ' && !letter)
+		{
+			strArr[counter] = first_word(str + counter);
+			if (!strArr[counter])
+			{
+				wordCount--;
+				while (wordCount >= 0)
+					free(*(strArr + wordCount--));
+				free(strArr);
+				return (NULL);
+			}
+			wordCount++;
+			letter = 1;
+		}
+		else if (str[counter] == ' ' && letter)
+			letter = 0;
+		counter++;
+	}
+	if (!wordCount)
+		return (NULL);
+
+	return (strArr);
+}
+
+/**
+ * word_count - Count number of words
+ * @str: char pointer
+ * Return: Word count
+ */
+
+int word_count(char *str)
+{
+	int counter = 0, wordCount, letter;
+
+	while (str[counter] != '\0')
+	{
+		if (str[counter] != ' ' && !letter)
+		{
+			wordCount++;
+			letter = 1;
+		}
+		else if (str[counter] == ' ' && letter)
+		{
+			letter = 0;
+		}
+		counter++;
+	}
+	return (wordCount);
+}
+
+/**
+ * first_word - Gets first word
+ * @str: char pointer
+ * Return: Pointer to word
+ */
+
+char *first_word(char *str)
+{
+	int counter;
+	char *word;
+
+	counter = 0;
+
+	while (str[counter] != ' ' && str[counter] != '\0')
+	{
+		counter++;
+	}
+
+	word = malloc(sizeof(char) * (counter + 1));
+
+	if (!word)
+	{
+		return (NULL);
+	}
+
+	word[counter] = '\0';
+
+	counter--;
+
+	while (counter >= 0)
+	{
+		word[counter] = str[counter];
+		counter--;
+	}
+	return (word);
+}
+
+
+
 	int i, k, n, word_count = 0, word_len = 0, current_word = 0;
 
 	if (str == NULL || *str == '\0')
