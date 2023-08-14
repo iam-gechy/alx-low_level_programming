4ef6a7526e360d60192f48431fd9c8977e3d195f
iam-gechy <onwukajudyth@gmail.com>
2023-08-14 14:21:45 -0700

    preprocessor tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints file name program was compiled from.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
